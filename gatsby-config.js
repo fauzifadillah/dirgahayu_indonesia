0eb6fbeaab4a1784489772ed903f6bbf8b26b83c
fauzifadillah <fauzifadillah00@yahoo.com>
2020-08-17 15:51:39 +0700

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Dirgahayu Indonesia`,
+        short_name: `HUT RI 75`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
